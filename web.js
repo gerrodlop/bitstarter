aed08a944f3a96f90380720a42b682a144f3fe01
grodrig <grodrig@casuca>
2013-07-10 00:15:02 +0200

    Lectura desde fichero.

--- web.js
+++ web.js
@@ -1,9 +1,24 @@
 var express = require('express');
 
+var fs = require('fs');
+var Buffer = require('buffer').Buffer;
+var constants = require('constants');
+
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+	fs.open("index.html", 'r', function(status, fd) {
+		if (status) {
+			console.log(status.message);
+			return;
+		}
+		var buffer = new Buffer(100);
+		fs.read(fd, buffer, 0, 100, 0, function(err, num) {
+			console.log(buffer.toString('utf-8', 0, num));
+  		response.send(buffer.toString('utf-8', 0, num));
+		});
+	});
+  //response.send('Hello World 2!');
 });
 
 var port = process.env.PORT || 5000;
